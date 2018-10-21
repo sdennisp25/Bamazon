b63d727aeafdf6e54420cef3aab3ee2cf1433508
Dennis The Menace <sdennisp25@hotmail.com>
2018-10-21 10:13:14 -0600

    got some more js. fixing why it loops twice

--- bamazon.sql
+++ bamazon.sql
@@ -3,9 +3,9 @@ CREATE database bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-	item_id INT NOT NULL,
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	product_name VARCHAR(50) NULL,
-	dapartment_name VARCHAR(50) NULL,
+	department_name VARCHAR(50) NULL,
 	price INT(10) NULL,
 	stock_quantity INT(100) NULL,
 	PRIMARY KEY (item_id)
@@ -14,32 +14,15 @@ CREATE TABLE products (
 SELECT * FROM products;
 
 
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("tires", "auto", 1, 50);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("cameras", "electronics", 200, 10);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("chairs", "living", 50, 50);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("picture frames", "art", 5, 100);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("coloring book", "craft", 3, 400);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("xbox", "entertainment", 300, 20);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("dishes", "kitchen", 2, 500);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("buzz lightyear", "toys", 9, 150);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("luggage", "travel", 80, 80);
-
-INSERT INTO products (product_name, dapartment_name, price, stock_quantity)
-VALUES ("necklace", "jewelry", 1000, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("tires", "auto", 1, 50),
+("cameras", "electronics", 200, 10),
+("chairs", "living", 50, 50),
+("picture", "art", 5, 100),
+("book", "craft", 3, 400),
+("xbox", "entertainment", 300, 20),
+("dishes", "kitchen", 2, 500),
+("buzz", "toys", 9, 150),
+("luggage", "travel", 80, 80),
+("necklace", "jewelry", 1000, 5);

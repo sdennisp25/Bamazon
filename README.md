200dbf3adfa777292ee8d587596e6566899ab05b
Dennis The Menace <sdennisp25@hotmail.com>
2018-10-27 17:10:54 -0600

    completed the readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Bamazon
+
+#### Welcome to my Bamazon! Here you will have to use terminal or gitbash along with MySQL Workbench to work. You will be given a list from the inventory. First you will be asked which item ID you would like to purchase. Following how many items you would like. If we have it in stock, you will receive a notice as well as the total amount from the purchase. Else, you will be told there is not any in stock.
+
+## Programs
+* MySQL Workbench
+* Javascript
+* Node JS
+
+## Command Rules
+* Type in the item ID you would like to purchase
+* Enter how many you would like
+
+
+# Images of the program
+![Alt text](/assets/images/bamwork1)
+![Alt text](/assets/images/bamwork2)
+![Alt text](/assets/images/bamnowork)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -57,22 +57,23 @@ function initialPurchase() {
 
 				connection.query(queryStr, { item_id: item }, function (err, data) {
 					if (err) throw err;
+
+					//test the condition to validate users choice for item ID
 					if (data.length === 0) {
 						console.log('ERROR: Please enter a valid Item ID.');
-						// displayInventory();
 					}
-
 					else {
 						console.log("\n-------------------------------------------------------------\n");
 						console.log("Item ID Validated!");
 					}
 
 					var productData = data[0];
+					//will run the conditions if the customers choice is a valid ID choice
 					if (quantity <= productData.stock_quantity) {
 						console.log('We have the item in stock!');
 						console.log("\n-------------------------------------------------------------\n");
 
-						//This will update the inventory
+						//This will update the inventory in products list
 						var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
 						connection.query(updateQueryStr, function (err, data) {
 							if (err) throw err;
@@ -96,11 +97,11 @@ function initialPurchase() {
 			})
 };
 
+//this displays the list of items in sql folder for products
 function displayInventory() {
 	queryStr = 'SELECT * FROM products';
 	connection.query(queryStr, function (err, data) {
 		console.log("\n");
-		var stringOutput;
 		for (var i = 0; i < data.length; i++) {
 			var stringOutput = "ID: " + data[i].item_id + " || Price: $" + data[i].price + " || Product: " + data[i].product_name + " || Department: " + data[i].department_name;
 
